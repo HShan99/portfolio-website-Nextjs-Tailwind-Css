34764adc777594d1037c86f8c17bf81f2ac30fc7
HShan99 <shanharsharathnayake@gmail.com>
2025-01-01 00:45:49 +0530

    Create the dark mode option

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,6 +2,7 @@ import { Poppins } from "next/font/google";
 import type { Config } from "tailwindcss";
 
 export default {
+  darkMode: "class",
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
