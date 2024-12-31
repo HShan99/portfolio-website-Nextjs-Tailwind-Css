5a1660ed0c45053d1addc586d691ddc985294dd3
HShan99 <shanharsharathnayake@gmail.com>
2024-12-31 13:00:52 +0530

    Create Nav bar

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,3 +1,4 @@
+import { Poppins } from "next/font/google";
 import type { Config } from "tailwindcss";
 
 export default {
@@ -8,6 +9,9 @@ export default {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        Poppins: ["poppins", "sans-serif"],
+      },
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
